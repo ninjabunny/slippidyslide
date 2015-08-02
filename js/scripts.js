88c542ecbc4348971d8bb60cf7336b4bf1f617dd
Anderson Tao <bunnydoctor@gmail.com>
2015-08-02 11:37:01 -0700

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// document.getElementById("container").style.height = 
+// 	Math.max(document.documentElement.clientHeight, window.innerHeight || 0) + "px";
+
+function f(){
+	document.getElementById("theInput").style.fontSize = 
+		Math.max(document.documentElement.clientHeight, window.innerHeight || 0) * 1.7 + "px";
+	document.getElementById("theInput").style.lineHeight = 
+		Math.max(document.documentElement.clientHeight, window.innerHeight || 0) + "px";	
+
+}
+f();
